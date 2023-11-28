8b7492a2f965f241ec5f2e0e9eebbd244d2485a1
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-11-28 16:01:55 +0500

    generate all data from products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,37 +1,7 @@
-const products = [
-    {
-        image: '/images/products/athletic-cotton-socks-6-pairs.jpg',
-        name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-        rating: {
-            stars: 4.5,
-            count: 87
-        },
-        pricCent: 1090
-    },
-    {
-        image: '/images/products/intermediate-composite-basketball.jpg',
-        name: 'Intermediate Size Basketball',
-        rating: {
-            stars: 4,
-            count: 127
-        },
-        pricCent: 2095
-    },
-    {
-        image: '/images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-        name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-        rating: {
-            stars: 4.5,
-            count: 56
-        },
-        pricCent: 799
-    },
-];
-
 let productsHTML = '';
 
 products.forEach((product)=> {
-    const {image, name, rating, pricCent} = product;
+    const {image, name, rating, priceCents} = product;
     productsHTML += `
         <div class="product-container">
         <div class="product-image-container">
@@ -52,7 +22,7 @@ products.forEach((product)=> {
         </div>
 
         <div class="product-price">
-        $${pricCent/100}
+        $${(priceCents/100).toFixed(2)}
         </div>
 
         <div class="product-quantity-container">
