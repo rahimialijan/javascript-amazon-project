93d4413bf584cb837c067508b5e6501fcd5c961d
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-12-01 14:47:58 +0500

    generate the order item to checkout page

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,19 @@
-export const cart = [];
+export const cart = [
+  {
+    id: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quentity: 2
+},
+{
+  id: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quentity: 1
+},
+
+{
+  id: '8c9c52b5-5a19-4bcb-a5d1-158a74287c53',
+  quentity: 3
+},
+
+];
 
 
 export function generateCart(productId){
